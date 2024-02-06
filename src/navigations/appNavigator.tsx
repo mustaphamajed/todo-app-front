40447a4d89432ef2feb011d170ded0896850def6
Mustapha Majed <mustapha.majed7@gmail.com>
2024-02-06 17:46:06 +0100

    fix(app navigator): auth logic
    return main stack if already authenticated

--- src/navigations/appNavigator.tsx
+++ src/navigations/appNavigator.tsx
@@ -1,26 +1,52 @@
+import React, { useEffect, useState } from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigatorScreenParams } from "@react-navigation/native";
 import { AuthNavigator, AuthStackParamList } from "./authNavigator";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { ROUTE_NAMES } from "../utils/routes";
 import { MainNavigator, MainStackParamList } from "./mainNavigator";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { ROUTE_NAMES } from "../utils/routes";
+import { STORAGE, getData } from "../utils/storage";
 
 export type RootStackParamList = {
   auth: NavigatorScreenParams<AuthStackParamList>;
   main: NavigatorScreenParams<MainStackParamList>;
 };
+
 const RootStack = createNativeStackNavigator<RootStackParamList>();
 
 export const AppNavigator = () => {
-  return (
-    <RootStack.Navigator screenOptions={{ headerShown: false }}>
-      <RootStack.Screen
-        name={ROUTE_NAMES.STACK.AUTH}
-        component={AuthNavigator}
-      />
-      <RootStack.Screen
-        name={ROUTE_NAMES.STACK.MAIN}
-        component={MainNavigator}
-      />
-    </RootStack.Navigator>
-  );
+  const [token, setToken] = useState<string | null>(null);
+
+  useEffect(() => {
+    const checkToken = async () => {
+      try {
+        const storedToken = await getData(STORAGE.accessToken);
+        setToken(storedToken);
+      } catch (error) {
+        console.error("Error reading token from AsyncStorage:", error);
+      }
+    };
+
+    checkToken();
+  }, []);
+
+  if (token) {
+    return (
+      <RootStack.Navigator screenOptions={{ headerShown: false }}>
+        <RootStack.Screen
+          name={ROUTE_NAMES.STACK.MAIN}
+          component={MainNavigator}
+        />
+      </RootStack.Navigator>
+    );
+  } else {
+    return (
+      <RootStack.Navigator screenOptions={{ headerShown: false }}>
+        <RootStack.Screen
+          name={ROUTE_NAMES.STACK.AUTH}
+          component={AuthNavigator}
+        />
+      </RootStack.Navigator>
+    );
+  }
 };
