a043be75af61757b383cb6abe85fadb9f2117edd
Mustapha Majed <mustapha.majed7@gmail.com>
2024-02-06 11:07:52 +0100

    feat(home-screen): stat section
    implement stat section

---
+++ src/components/shared/statContainer.tsx
@@ -0,0 +1,41 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import commonStyles from "../../styles/commonStyles";
+
+const StatContainer = () => {
+  const stats = [
+    { id: 1, label: "Overdue", count: 421 },
+    { id: 2, label: "To Do", count: 81 },
+    { id: 3, label: "Open", count: 72 },
+    { id: 4, label: "Due Today", count: 51 },
+  ];
+  return (
+    <View
+      style={[
+        commonStyles.w100,
+        commonStyles.bgWhite,
+        commonStyles.p20,
+        commonStyles.br8,
+        commonStyles.row,
+        commonStyles.justifyAround,
+        { elevation: 5 },
+      ]}
+    >
+      {stats.map(({ count, id, label }) => {
+        return (
+          <View
+            key={id}
+            style={[commonStyles.justifyCenter, commonStyles.alignCenter]}
+          >
+            <Text>{count}</Text>
+            <Text>{label}</Text>
+          </View>
+        );
+      })}
+    </View>
+  );
+};
+
+export default StatContainer;
+
+const styles = StyleSheet.create({});
