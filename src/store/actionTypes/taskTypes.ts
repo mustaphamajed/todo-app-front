60f14d1206088879b86569aac12b809419f21e35
Mustapha Majed <mustapha.majed7@gmail.com>
2024-02-11 22:48:26 +0100

    feat(task): task type
    add task type

--- src/store/actionTypes/taskTypes.ts
+++ src/store/actionTypes/taskTypes.ts
@@ -2,10 +2,17 @@ export const FETCH_TASKS_LOADING = "FETCH_TASKS_LOADING";
 export const FETCH_TASKS_SUCCESS = "FETCH_TASKS_SUCCESS";
 export const FETCH_TASKS_FAILURE = "FETCH_TASKS_FAILURE";
 
+export const ADD_TASK_LOADING = "ADD_TASK_LOADING";
+export const ADD_TASK_SUCCESS = "ADD_TASK_SUCCESS";
+export const ADD_TASK_FAILURE = "ADD_TASK_FAILURE";
+
 export enum taskActionTypes {
   FETCH_TASKS_LOADING = "FETCH_TASKS_LOADING",
   FETCH_TASKS_SUCCESS = "FETCH_TASKS_SUCCESS",
   FETCH_TASKS_FAILURE = "FETCH_TASKS_FAILURE",
+  ADD_TASK_LOADING = "ADD_TASK_LOADING",
+  ADD_TASK_SUCCESS = "ADD_TASK_SUCCESS",
+  ADD_TASK_FAILURE = "ADD_TASK_FAILURE",
 }
 
 interface LoadingFetchAction {
@@ -21,7 +28,23 @@ interface FetchFailedAction {
   type: typeof taskActionTypes.FETCH_TASKS_FAILURE;
 }
 
+interface LoadingAddAction {
+  type: typeof taskActionTypes.ADD_TASK_LOADING;
+}
+
+interface AddSuccessAction {
+  type: typeof taskActionTypes.ADD_TASK_SUCCESS;
+  payload: any;
+}
+
+interface AddFailedAction {
+  type: typeof taskActionTypes.ADD_TASK_FAILURE;
+}
+
 export type TaskActionTypes =
   | LoadingFetchAction
   | FetchSuccessAction
-  | FetchFailedAction;
+  | FetchFailedAction
+  | LoadingAddAction
+  | AddSuccessAction
+  | AddFailedAction;
